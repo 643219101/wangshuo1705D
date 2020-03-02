c08e2bf35f93ee8134b8a38d6ed32f29666e6a71
643219101 <643219101@qq.com>
2020-03-02 20:29:47 +0800

    管理员根据ID回显前端

---
+++ opencartback-front/viewmodels/administratorUpdateProfile.js
@@ -0,0 +1,38 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        username:'',
+        realName:'',
+        email:'',
+        avatarUrl:'',
+        createTimestamp:''      
+    },
+    mounted() {
+        console.log('view mounted');
+        this.getMyProfile();
+    },
+   methods:{
+    handleUpdateClick() {
+        console.log('update click');
+        this.updateMyProfile();
+    },
+        getMyProfile(){
+            axios.get('/administrator/getProfile')
+            .then(function (response) {
+                console.log(response);
+                var me = response.data;
+                app.administratorId = me.administratorId;
+                app.username = me.username;
+                app.realName = me.realName;
+                app.email = me.email;
+                app.avatarUrl = me.avatarUrl;
+                app.createTimestamp = me.createTimestamp;
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+        }
+
+   }
+})
