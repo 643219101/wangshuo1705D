60abb17ee3232d7aecc6dcf010d426f697865847
643219101 <643219101@qq.com>
2020-03-04 16:19:04 +0800

    购物车VUE 删除和修改

--- opencartstore-front/viewmodels/shoppingCart.js
+++ opencartstore-front/viewmodels/shoppingCart.js
@@ -4,10 +4,28 @@ var app = new Vue({
         myShoppingCart:[]
 
     },
+    methods:{ 
+        handleUpdate(index, row){
+            console.log('update click');
+            localStorage['myShoppingCartJson'] = JSON.stringify(this.myShoppingCart);
+            this.$message.success('修改购物车成功');
+        },
+        handleDelete(index, row){
+            if (confirm('确定删除？')) {
+                //删除localstorage 根据下标 的数据
+                this.myShoppingCart.splice(index, 1);
+                localStorage['myShoppingCartJson'] = JSON.stringify(this.myShoppingCart);
+                this.$message.success('删除购物车成功');
+            }
+
+        }
+    },
     mounted(){
                console.log('shopping  cart')
                var myShoppingCartJson = localStorage['myShoppingCartJson'];
                this.myShoppingCart = myShoppingCartJson ? JSON.parse(myShoppingCartJson) : [];
 
+
     }
+    
 })
