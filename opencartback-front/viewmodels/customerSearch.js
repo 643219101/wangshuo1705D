578633161e71f5f35f156cc842a3ed8d5e6e32ce
643219101 <643219101@qq.com>
2020-03-07 20:23:41 +0800

    管理段      修改用户的状态  前端

--- opencartback-front/viewmodels/customerSearch.js
+++ opencartback-front/viewmodels/customerSearch.js
@@ -2,7 +2,12 @@ var app = new Vue({
     el: '#app',
     data: {
         pageInfo:'',
-        pageNum:1 
+        pageNum:1,
+        statuses: [
+            { value: 0, label: '禁用' },
+            { value: 1, label: '启用' },
+            { value: 2, label: '不安全' }
+        ]
 
     },
     mounted(){
@@ -10,8 +15,12 @@ var app = new Vue({
     },
     methods:{
         pageChange(val){
-  this.pageNum=val;
-  this.searchCutomer();
+          this.pageNum=val;
+          this.searchCutomer();
+
+        },
+        handleUpdateStatus(index,row){
+          this.updateCustomerStatus(row.customerId, row.status);
 
         },
          searchCutomer(){
@@ -26,6 +35,19 @@ var app = new Vue({
               console.log(error);
             })
          
-         }
+         },
+         updateCustomerStatus(customerId, status) {
+          axios.post('/customer/setStatus', {
+              customerId: customerId,
+              status: status
+          })
+              .then(function (response) {
+                  console.log(response);
+                  alert('状态更新成功');
+              })
+              .catch(function (error) {
+                  console.log(error);
+              });
+      }
     }
 })
